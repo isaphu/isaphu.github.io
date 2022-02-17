e549c2fdab9f37edbab573b91e27515498884689
Isa Phuyuthanon <uronohan@gmail.com>
2022-02-17 13:46:27 +0700

    adding file

---
+++ intro-to-js/script.js
@@ -0,0 +1,12 @@
+function askMyName() {
+  const name = prompt("What's your name?");
+
+  if (name != null) {
+    document.getElementById("my-name").innerHTML = "My name is " + name;
+  }
+}
+
+function helloWorld() {
+  const h1message = document.getElementById("my-name").innerText
+  alert(h1message + " หยาดเหงือทุกหยดอนาคตของลูก")
+}
