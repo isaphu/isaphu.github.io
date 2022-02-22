b40620acff7a18b5b600283b9938887094507639
Isa Phuyuthanon <uronohan@gmail.com>
2022-02-22 10:43:34 +0700

    adding file

---
+++ intro-to-js/main.js
@@ -0,0 +1,47 @@
+//22-FEB-2022 'Control-Flow'
+
+const printGroupName = (groupNo) => {
+    let groupName;
+  
+    if(groupNo === 1) {
+      groupName = 'Pokki'
+    } else if (groupNo === 2) {
+      groupName = 'ปาท๋องโก๋'
+    } else if (groupNo === 3) {
+      groupName = 'ติวกระตุกจิต'
+    } else if (groupNo === 4) {
+      groupName = 'สี่ไม่ซี้'
+    } else if (groupNo === 5) {
+      groupName = 'Laugh Tale'
+    } else if (groupNo === 6) {
+      groupName = 'ซานตา'
+    } else if (groupNo === 7) {
+      groupName = 'สายลับ เจมส์บอนด์'
+    } else if (groupNo === 8) {
+      groupName = 'Infinity'
+    } else if (groupNo === 10) {
+      groupName = 'Ben Ten'
+    } else if (groupNo === 11) {
+      groupName = 'เต้าหู้'
+    } else if (groupNo === 12) {
+      groupName = 'แป้งเย็น'
+    } else {
+      groupName = 'พี่นัน'
+    }
+    console.log('Your Group Name =', groupName)
+  }
+
+  printGroupName(1)
+  printGroupName(2)
+  printGroupName(3)
+  printGroupName(4)
+  printGroupName(5)
+  printGroupName(6)
+  printGroupName(7)
+  printGroupName(8)
+  printGroupName(9)
+  printGroupName(10)
+  printGroupName(11)
+  printGroupName(12)
+  printGroupName(23)
+  printGroupName(40)
