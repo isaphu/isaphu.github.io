06e765332ff8277aeee27ea0be2ce10e46c6d652
Isa Phuyuthanon <uronohan@gmail.com>
2022-02-25 22:45:30 +0700

    adding file

--- intro-to-js/vending-machine.js
+++ intro-to-js/vending-machine.js
@@ -1,5 +1,35 @@
 //implementing vending machine
 
-//รับเงิน
+//1 รับเหรียญ
 const coins = prompt('Please insert your coins');
 console.log('coins',coins);
+const coinArray = coins.split(' ')
+console.log('coinArray', coinArray)
+
+//2 เช๊คเหรียญ และคำนวนเงิน
+const coinValue = [1,2,5,10]
+
+let amount = 0;
+for(let i = 0; i < coinArray.length; i++) {
+    amount += coinArray[i] * coinValue[i]
+}
+
+console.log('amount',amount)
+
+//เลือกของ
+const products = ['coke', 'water', 'm150','coffee', 'code zero','vodka'];
+const prices = [13,10,10,50,13,100]
+//1 รับ input เบอร์จาก user
+const productInputIndex = prompt('Please select product (0-5)')
+
+console.log('productInputIndex', productInputIndex)
+
+//2 เลือกจำนวน
+const quantity = Number(prompt('Please type quantity'))
+console.log('quantity', quantity)
+
+//3 คำนวนราคาสุทธิ
+const selectedPrice = prices[productInputIndex]
+console.log('selectedPrice', selectedPrice)
+
+//เช็คราคา
