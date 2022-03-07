359f76258176bf806940c509b76c98bb7402517c
Isa Phuyuthanon <uronohan@gmail.com>
2022-03-07 14:45:26 +0700

    adding axios

---
+++ axios/main.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const run = async () => {
+    // const responseBitcoint = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDC')
+    // const responseDogecoint = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT')
+    // const igSearch = await axios.get('http://api.instagram.com/oembed?url=http://instagram.com/p/Y7GF-5vftL/')
+    // const igComment = await axios.get('https://api.instagram.com/v1/media/448979387270691659_45818965/comments')
+    // console.log(responseBitcoint.data)
+    // console.log(responseDogecoint.data)
+    // console.log(igSearch.data)
+    // console.log(igComment.data)
+}
+
+
+run();
